fc32f84247c0cb82db816f6cb21acf090c2414f8
Kris Borchers <kris.borchers@gmail.com>
2016-03-22 14:45:04 -0400

    Cleanup and add ability to check only PR authors

--- index.js
+++ index.js
@@ -4,33 +4,19 @@ var child = require('child_process');
 var fs = require('fs');
 var parse = require('peoplestring-parse');
 var path = require('path');
+var github = require('github-request');
 
-function checkAUTHORSCertificate(directory, callback) {
-  listGitAuthors(directory, function(error, gitAuthors) {
-    if (error) {
-      callback(error);
-    } else {
-      listAUTHORS(directory, function(error, authors) {
-        if (error) {
-          callback(error);
-        } else {
-          var missing = gitAuthors
-            .filter(function(gitAuthor) {
-              return ( authors.indexOf(gitAuthor) < 0 );
-            });
-          callback(null, missing);
-        }
-      });
-    }
-  });
+function filterAuthors(gitAuthors, authors) {
+  return gitAuthors
+    .filter(function(gitAuthor) {
+      return ( authors.indexOf(gitAuthor) < 0 );
+    });
 }
 
-function listGitAuthors(gitDirectory, callback) {
+function gitCommand(options) {
   var childError = false;
 
-  var git = child.spawn(
-    'git', [ 'log',  '--format=%aN' ],
-    { cwd: gitDirectory });
+  var git = child.spawn('git', options.commandArgs, options.commandOptions);
 
   var outputBuffers = [];
   git.stdout
@@ -47,35 +33,83 @@ function listGitAuthors(gitDirectory, callback) {
   git
     .on('error', function(error) {
       childError = error;
-    });
+    })
     .on('close', function(exitCode) {
       if (exitCode !== 0) {
-        callback(
+        options.callback(
           new Error(
             'Failed with exit code ' + exitCode + ':\n' +
             Buffer.concat(errorBuffers).toString()
-          );
+          )
         );
       } else if (childError) {
-        callback(childError)
+        options.callback(childError)
       } else {
-        var gitAuthors = Buffer.concat(outputBuffers)
-          .toString()
-          .split('\n')
-          .reduce(
-            function(unique, element) {
-              var empty = ( element.trim().length === 0 );
-              var seen = ( unique.indexOf(element) > -1 );
-              return ( ( !seen && !empty ) ?
-                unique.concat(element) :
-                unique
-              )
-            }, []);
-
-        callback(null, gitAuthors);
+        options.nextStep(outputBuffers, options.callback);
       }
     }
   );
+}
+
+function checkAUTHORSCertificate(isPR, directory, callback) {
+  listCorrectGitAuthors(isPR, directory, function(error, gitAuthors) {
+    if (error) {
+      callback(error);
+    } else {
+      listAUTHORS(directory, function(error, authors) {
+        if (error) {
+          callback(error);
+        } else {
+          var missing = [];
+          if (isPR) {
+            missing = filterAuthors(authors, gitAuthors)
+          } else {
+            missing = filterAuthors(gitAuthors, authors);
+          }
+          callback(null, missing);
+        }
+      });
+    }
+  });
+}
+
+function listCorrectGitAuthors(isPR, directory, callback) {
+  if (isPR) {
+    github.request({
+      path: '/repos/' + process.env.TRAVIS_REPO_SLUG + '/pulls/' + process.env.TRAVIS_PULL_REQUEST
+    }, function(error, pr) {
+      gitCommand({
+        commandArgs: [ 'clone', pr.head.repo.clone_url, "pr-repo"  ],
+        commandOptions: { cwd: directory + '../' },
+        callback: callback,
+        nextStep: listGitAuthors
+      });
+    });
+  } else {
+    gitCommand({
+      commandArgs: [ 'log',  '--format=%aN' ],
+      commandOptions: { cwd: directory },
+      callback: callback,
+      nextStep: listGitAuthors
+    });
+  }
+}
+
+function listGitAuthors(outputBuffers, callback) {
+  var gitAuthors = Buffer.concat(outputBuffers)
+    .toString()
+    .split('\n')
+    .reduce(
+      function(unique, element) {
+        var empty = ( element.trim().length === 0 );
+        var seen = ( unique.indexOf(element) > -1 );
+        return ( ( !seen && !empty ) ?
+          unique.concat(element) :
+          unique
+        )
+      }, []);
+
+  callback(null, gitAuthors);
 }
 
 var commentRE = /^\s*#/;
